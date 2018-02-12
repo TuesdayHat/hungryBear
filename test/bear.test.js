01156145e99e0940e972f6391fdb9a848375fe70
Adam Calhoun and James Donlan <adam@>
2018-02-12 09:37:19 -0800

    test bear hunger events

--- test/bear.test.js
+++ test/bear.test.js
@@ -1,7 +1,7 @@
 import {HungryBear} from './../src/bear.js';
 import * as sinon from 'sinon';
 const assert = require('assert');
-const babel = require ('babel-core');
+// const babel = require ('babel-core');
 const expect = require('chai').expect
 
 describe('Bear', function() {
@@ -10,6 +10,7 @@ describe('Bear', function() {
 
   beforeEach(function(){
     clock = sinon.useFakeTimers();
+    fuzzy.setHunger();
   });
   afterEach(function () {
     clock.restore();
@@ -19,6 +20,27 @@ describe('Bear', function() {
   it('should have a name and a food level of 10 when it is created', function() {
       expect(fuzzy.name).to.equal("Fuzzy");
       expect(fuzzy.foodLevel).to.equal(10);
+  });
+
+  it('should have a food level of 7 after 3001 milliseconds', function() {
+    clock.tick(3001);
+    expect(fuzzy.foodLevel).to.equal(7);
+  });
+
+  it('should get very hungry if the food level drops below zero', function() {
+    fuzzy.foodLevel = 0;
+    expect(fuzzy.didYouGetEaten()).to.equal(true);
+  });
+
+  it('should get very hungry if 10 seconds pass without feeding', function() {
+    clock.tick(10001);
+    expect(fuzzy.didYouGetEaten()).to.equal(true);
+  });
+
+  it('should have a food level of ten if it is fed', function() {
+    clock.tick(9001);
+    fuzzy.feed();
+    expect(fuzzy.foodLevel).to.equal(10);
   });
   // describe('', function(){
   //   it('should do the thing', function(){
